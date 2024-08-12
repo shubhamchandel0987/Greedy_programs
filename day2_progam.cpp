d3c1f8ee1b4376ba92a4b76cd683949077cc61ff
shubhamchandel0987 <146477807+shubhamchandel0987@users.noreply.github.com>
2024-08-12 10:53:35 +0530

    Update day2_progam.cpp

--- day2_progam.cpp
+++ day2_progam.cpp
@@ -1,46 +1,62 @@
-//climbing stairs leetcode problem 70
+//The coin change problem leetcode 322
 
 class Solution {
 public:
-    int climbStairs(int n) 
-    {
-       vector<int> dp(n+1,0);
-       dp[0]=1;
-       dp[1]=1;
 
-       for(int i=2;i<=n;i++)
-       {
-        dp[i]=dp[i-1]+dp[i-2];
-       }
-       return dp[n];
+    int solve(vector<int>&coins,int amount,vector<int> &dp)
+    {
+        dp[0]=0;
+
+        for(int i=1;i<=amount;i++)
+        {
+            for(int j=0;j<coins.size();j++)
+            {
+                if(i-coins[j]>=0 && dp[i-coins[j]]!=100000)
+                dp[i]=min(dp[i],1+dp[i-coins[j]]);
+            }
+        }
+
+        return dp[amount];
+    }
+    int coinChange(vector<int>& coins, int amount) 
+    {
+
+       vector<int> dp(amount+1,100000);
+       int ans=solve(coins,amount,dp);
+       if(ans!=100000)
+       return ans;
+       else
+       return -1;
     }
 };
 
-//House robber leetcode problem 198
+//Longest increasing subsequence leetcode 300
 
 class Solution {
 public:
-    int house_rob(vector<int>& nums,int n,vector<int> &dp)
+    int solve(vector<int> &nums,int curr,int prev,vector<vector<int>> &dp)
     {
-        if(n<0)
-          return 0;
-        if(n==0)
-        {
-            return nums[0];
-        }
-        if(dp[n]!=-1)
-           return dp[n];
+        if(curr==nums.size())
+         return 0;
 
-        return dp[n]=max(house_rob(nums,n-2,dp)+nums[n],house_rob(nums,n-1,dp)+0);
+        if(dp[curr][prev+1]!=-1)
+        return dp[curr][prev+1];
+
+        int take=0;
+        if(prev==-1 || nums[curr]>nums[prev])
+        take=1+solve(nums,curr+1,curr,dp);
 
 
+        int not_take=0+solve(nums,curr+1,prev,dp);
+
+        return dp[curr][prev+1]=max(take,not_take);
     }
-    int rob(vector<int>& nums) 
+    int lengthOfLIS(vector<int>& nums) 
     {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
-        int res= house_rob(nums,n-1,dp);
-        return res;
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+       int ans=solve(nums,0,-1,dp);
+       return ans;
+        
     }
-
 };
